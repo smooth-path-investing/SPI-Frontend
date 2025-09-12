f49df5f5970ddca15a25a1295c2dc0b9b3d1ebee
Jamil Shoujah <jamilshoujah@gmail.com>
2025-09-12 11:29:18 +0000

    Refactor Home page for clarity

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { AnimatedKeywords } from '../components/ui/animated-keywords';
-import { MissionSection } from '../components/home/MissionSection';
 import { HeroSection } from '../components/home/HeroSection';
-import { PerformanceSection } from '../components/home/PerformanceSection';
 import { MethodologySection } from '../components/home/MethodologySection';
-import { TrustSection } from '../components/home/TrustSection';
+import { PerformanceSection } from '../components/home/PerformanceSection';
 import { WhyChooseUsSection } from '../components/home/WhyChooseUsSection';
-import { AboutUsSection } from '../components/home/AboutUsSection';
+import { TrustSection } from '../components/home/TrustSection';
 
 export const Home: React.FC = () => {
   return (
-    <div className="min-h-screen bg-background text-foreground relative">
-      <AnimatedKeywords />
-      <MissionSection />
+    <div className="min-h-screen bg-background text-foreground">
       <HeroSection />
-      <PerformanceSection />
       <MethodologySection />
-      <TrustSection />
+      <PerformanceSection />
       <WhyChooseUsSection />
-      <AboutUsSection />
+      <TrustSection />
     </div>
   );
 };
