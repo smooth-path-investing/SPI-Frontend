77b640cfe5d24a2c94a85191253e77366225a7c9
Jamil Shoujah <jamilshoujah@gmail.com>
2025-10-02 11:17:23 +0000

    Rebuild portfolio backtest feature

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { About } from "./pages/About";
 import { Approach } from "./pages/Approach";
 import { Performance } from "./pages/Performance";
 import { Stocks } from "./pages/Stocks";
+import { Backtest } from "./pages/Backtest";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -31,6 +32,7 @@ const App = () => (
                 <Route path="/approach" element={<Approach />} />
                 <Route path="/performance" element={<Performance />} />
                 <Route path="/stocks" element={<Stocks />} />
+                <Route path="/backtest" element={<Backtest />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
