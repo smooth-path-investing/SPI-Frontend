0438c7d5725d1914d7c90b6bb2a66ecba6497243
JamilShoujah <jws09@mail.aub.edu>
2025-12-04 16:52:09 +0200

    fix 2

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -49,7 +49,7 @@ export const Navigation: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16 gap-4">
             {/* to be changed when logo is updated */}
-            <img src="public/images/SPI.png" alt={''} className="w-8 h-8 object-contain" />
+            <img src="images/SPI.png" alt={''} className="w-8 h-8 object-contain" />
 
             <div className="hidden md:flex space-x-6 flex-1 justify-center">
               {navigationItems.map((item) => (
