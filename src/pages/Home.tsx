fb502131e670887c35ed6386b5a0dc1f2d25784c
JamilShoujah <jws09@mail.aub.edu>
2026-02-08 15:05:36 +0200

    rest

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,17 @@
-import React from 'react';
-import { HeroSection } from '../components/home/HeroSection';
-import { MissionSection } from '../components/home/MissionSection';
-import { StatsSection } from '../components/home/StatsSection';
-import { MethodologySection } from '../components/home/MethodologySection';
-import { PerformanceSection } from '../components/home/PerformanceSection';
-import { AnimatedSection } from '@/components/animations/animationSection';
+import { HeroSection } from '@/components/home/HeroSection';
+import { MethodologySection } from '@/components/home/MethodologySection';
+import { MissionSection } from '@/components/home/MissionSection';
+import { PerformanceSection } from '@/components/home/PerformanceSection';
+import { StatsSection } from '@/components/home/StatsSection';
 
 export const Home: React.FC = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <AnimatedSection>
-        <HeroSection />
-      </AnimatedSection>
-
-      <AnimatedSection>
-        <MissionSection />
-      </AnimatedSection>
-
-      <AnimatedSection>
-        <MethodologySection />
-      </AnimatedSection>
-
-      <AnimatedSection>
-        <PerformanceSection />
-      </AnimatedSection>
-
-      <AnimatedSection>
-        <StatsSection />
-      </AnimatedSection>
+      <HeroSection />
+      <MissionSection />
+      <MethodologySection />
+      <PerformanceSection />
+      <StatsSection />/
     </div>
   );
 };
